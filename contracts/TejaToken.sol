bb23590ee0b443e545ab017421346bb1a183e8b2
amrdehghan1377 <31191650+amrdehghan1377@users.noreply.github.com>
2018-08-20 13:00:05 +0430

    2. Transfer Tokens

--- contracts/TejaToken.sol
+++ contracts/TejaToken.sol
@@ -2,11 +2,35 @@ pragma solidity ^0.4.2;
 
 contract TejaToken{
 
+    string public name = "TejaToken";
+    string public symbol = "TJT";
+    string public standard = "TejaToken v1.0";
 
     uint256 public totalSupply;
-    
-    function TejaToken() public {
-        totalSupply = 1000000 ;
+
+    event Transfer(
+        address indexed _from , 
+        address indexed _to , 
+        uint256 _value
+    );
+
+    mapping(address => uint256 ) public balanceOf ; 
+
+    function TejaToken(uint256 _initialSupply) public {
+        balanceOf[msg.sender] = _initialSupply;
+        totalSupply = _initialSupply ;
 
     }
+
+    function transfer(address _to , uint256 _value) public returns (bool success){  
+    
+        require(balanceOf[msg.sender] >= _value) ;
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+
+        Transfer(msg.sender , _to , _value);
+
+        return true ;
+    }
 }
+
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 var TejaToken = artifacts.require("./TejaToken.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(TejaToken);
+  deployer.deploy(TejaToken , 1000000);
 };
