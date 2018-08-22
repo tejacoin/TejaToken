2ce9b211149124e21e30e9cb05789e4b44325252
amrdehghan1377 <31191650+amrdehghan1377@users.noreply.github.com>
2018-08-22 10:51:37 +0430

    3. Delegated Transfer

--- contracts/TejaToken.sol
+++ contracts/TejaToken.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.4.2;
 contract TejaToken{
 
     string public name = "TejaToken";
-    string public symbol = "TJT";
+    string public symbol = "TJTN";
     string public standard = "TejaToken v1.0";
 
     uint256 public totalSupply;
@@ -12,9 +12,17 @@ contract TejaToken{
         address indexed _from , 
         address indexed _to , 
         uint256 _value
+    );  
+
+    event Approval(
+        address indexed _owner , 
+        address indexed _spender , 
+        uint256 _value
     );
 
+
     mapping(address => uint256 ) public balanceOf ; 
+    mapping(address => mapping(address => uint256)) public allowance ;
 
     function TejaToken(uint256 _initialSupply) public {
         balanceOf[msg.sender] = _initialSupply;
@@ -32,5 +40,29 @@ contract TejaToken{
 
         return true ;
     }
+
+    function approve (address _spender , uint256 _value) public returns (bool success){
+
+        allowance[msg.sender][_spender] = _value ;
+
+        Approval(msg.sender, _spender, _value);
+
+        return true ;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+
+        allowance[_from][msg.sender] -= _value;
+
+        Transfer(_from, _to, _value);
+
+        return true;
+    }
 }
 
+ 
